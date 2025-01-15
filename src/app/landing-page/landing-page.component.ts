3d2db2179f2d373cf012e501cd6e263cd4e2f119
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-14 23:06:25 -0300

    Boton de hamburguesa

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export default class LandingPageComponent {
+
+  menuOpen: boolean = false;
+
+  toggleMenu():void{
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
