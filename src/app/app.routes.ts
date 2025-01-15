3dd403c1b87ee8c91fbcad4cd1466c759cfe53d9
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-14 22:53:28 -0300

    Rutas y quite boostrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./landing-page/landing-page.component')
+  }
+];
