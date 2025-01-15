2774060060df083538d5a37b55f5bc7400d5b0b5
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-14 23:22:09 -0300

    Spinner de flash

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import SpinnerComponent from "../shared/pages/spinner/spinner.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SpinnerComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export default class LandingPageComponent {
 
+  loading: boolean = true;
   menuOpen: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;  // Después de 5 segundos, ocultamos el spinner
+    }, 5000);
+  }
 
   toggleMenu():void{
     this.menuOpen = !this.menuOpen;
