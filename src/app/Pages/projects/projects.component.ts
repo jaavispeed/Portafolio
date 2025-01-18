4556c8b3722ace57e49bd06697c4a66e357c1b90
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-18 02:59:08 -0300

    Mejorando el proyectos

--- src/app/Pages/projects/projects.component.ts
+++ src/app/Pages/projects/projects.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export default class ProjectsComponent {
+  isModalOpen: boolean = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
